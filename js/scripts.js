16d78daa8f918c4d60902ea2c75d7bd3d313a4d6
Blake Scott and Michael Schennum <mikeschenphoto@gmail.com>
2016-01-25 14:47:22 -0800

    add functionality to strikethrough list item when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
 function Task(taskName) {
-  this.taskName = taskName;
-
+    this.taskName = taskName;
 }
+
+// Task.prototype.strike = function() {
+//   return this.locationName + ", " + this.landmarkName + ", " + this.timeYearName;
+// }
+
+$(document).ready(function() {
+  $("form#new-list").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedTask = $("input#new-task").val();
+    var newTask = new Task(inputtedTask);
+
+    $("ul#task").append("<li><span class='task'>" + newTask.taskName + "</span></li>");
+
+    $(".task").click(function() {
+      $(this).parents("li").css("text-decoration", "line-through");
+    });
+
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,11 @@ describe ('toDoList', function() {
     var testTask = new Task("Do the Dishes");
     expect(testTask.taskName).to.equal("Do the Dishes");
   });
+
+  it("adds a strikethrough that appears when the user clicks on the task", function() {
+    var str = "Do the Dishes"
+    var testStrike = new Task(str.strike());
+    expect(testStrike.str).to.equal(str.strike());
+  });
+
 });
