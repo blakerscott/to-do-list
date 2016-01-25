1fd15e9ead4f294f768d066be0e26d8ed8d22359
Blake Scott and Michael Schennum <blakerscott@gmail.com>
2016-01-25 15:05:40 -0800

    add form clear

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,14 @@ function Task(taskName) {
 
 $(document).ready(function() {
   $("form#new-list").submit(function(event) {
+
     event.preventDefault();
 
     var inputtedTask = $("input#new-task").val();
     var newTask = new Task(inputtedTask);
 
     $("ul#task").append("<li><span class='task'>" + newTask.taskName + "</span></li>");
-
+    $("form").trigger("reset");
     $(".task").click(function() {
       $(this).parents("li").css("text-decoration", "line-through");
     });
